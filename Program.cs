dc324b014af2873c8fba529bd8470794e03b6304
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 19:56:25 +0300

    Case13 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программубкоторая выводит третью цифру числа или сообщает ,что ее нет
+int N = int.Parse(Console.ReadLine());
+if (N / 100 <1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int k = 1;
+    int N1 = N/10;
+    while (N1>0)
+        {
+            N1=N1/10;
+            k=k+1;
+        }
+    Console.WriteLine((N-N%Math.Pow(10,(k-3)))/Math.Pow(10,(k-3))%10);
+}
+
